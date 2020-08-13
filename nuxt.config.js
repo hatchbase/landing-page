0289e405e824db5f8cf4b4da2e30ceda400d3052
Daniel da Rocha <danrocha@gmail.com>
2020-08-13 15:24:51 +0200

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'HATCHBASE - the platform for collaboration in architecture',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -28,7 +28,7 @@ export default {
       },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      // { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'preconnect',
         href: 'https://fonts.gstatic.com',
